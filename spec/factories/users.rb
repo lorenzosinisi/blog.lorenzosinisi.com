c7b0bf4d9a9a1aab78fd155adaf70b8571057ce5
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-27 23:07:55 +0100

    railsadmin is now working

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    password Faker::Internet.password
+    email Faker::Internet.email
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe User do
+  
+  let(:user) { FactoryGirl.build(:user) }
+
+  it "should have admin attribute set to false by default" do
+    expect(user.admin).to be false
+  end
+
+  describe "#admin?" do 
+    it "should be true when the user is an admin" do 
+      user.admin = true
+      expect(user.admin?).to be true
+    end
+    it "false if it is a normal user" do
+      expect(user.admin?).to be false
+    end
+  end
 end
