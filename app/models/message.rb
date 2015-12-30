b3f111c5a9af59e388027a4b761a0616bd30aff9
Lorenzo Sinisi <lasslo.net@gmail.com>
2015-12-30 15:32:13 +0100

    fixed tests

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,18 @@
 class Message < ActiveRecord::Base
+  
+  # get the twilio sid
+  def self.twilio_sid
+    Rails.application.secrets.twilio_sid
+  end
+
+  # get the twilio secret token
+  def self.twilio_secret
+    Rails.application.secrets.twilio_secret
+  end
+
+  # initialize the client
+  def self.twilio_client
+    @client = Twilio::REST::Client.new twilio_sid, twilio_secret
+  end
+
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
+  let (:message) { Message }
+  
+  before(:each) do
+    Rails.application.secrets.twilio_sid = "ciao"
+    Rails.application.secrets.twilio_secret = "thisisasecret"
+  end
+
+  context "twilio methods" do 
+    it "#twilio_sid" do
+      expect(message.twilio_sid).to eq "ciao"
+    end
+    it "#twilio_secret" do
+      expect(message.twilio_secret).to eq "thisisasecret"
+    end
+    it "#twilio_client" do
+      expect(message.twilio_client).to be_a Twilio::REST::Client
+    end
+  end
+
 end
